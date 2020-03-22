baad474bb77b025bb4a25ae8f2abfb4ed804faf1
95kim2 <44775564+95kim2@users.noreply.github.com>
2020-03-22 10:03:12 +0900

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import requests
+import random
+import time
+
+for i in range(10):
+    val = random.randrange(0, 51)
+    url = 'https://api.thingspeak.com/update?api_key=W12BCJ9N07XFBZ3N&field1='+str(val)
+    r = requests.get(url)
+    time.sleep(30)
