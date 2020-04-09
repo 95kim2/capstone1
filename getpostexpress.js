a70fdbfa4b2d83af6479e6fc3038a9e1d29ccc5e
95kim2 <44775564+95kim2@users.noreply.github.com>
2020-04-09 21:42:04 +0900

    get post 파라미터 json형식으로 응답

---
+++ getpostexpress.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+require('date-utils')
+port = 8000
+
+// http://localhost:8000/capston2020?name=kim&color=red
+app.get('/capston2020', function(req, res) {
+    r = req.query
+    r.ip = req.ip.replace(/^.*:/, '')
+    r.time = (new Date()).toFormat("YYYY-MM-DD HH:MI:SS")
+    r.email = "temp@gmail.com"
+    r.stuno = "20202030"
+    console.log(r)
+    res.send(JSON.stringify(r))
+})
+
+app.post('/capston2020', function(req, res) {
+    r = req.body
+    r.ip = req.ip.replace(/^.*:/, '')
+    r.time = (new Date()).toFormat("YYYY-MM-DD HH:MI:SS")
+    r.email = "temp@gmail.com"
+    r.stuno = "20202030"
+    console.log(req.body)
+    res.send(JSON.stringify(r))
+})
+
+// http://localhost:8000/capston2020/kim/red
+app.get('/capston2020/:a/:b', function(req, res) {
+    r = req.params
+    ip = req.ip.replace(/^.*:/, '')
+    r.time = (new Date()).toFormat("YYYY-MM-DD HH:MI:SS")
+    r.email = "temp@gmail.com"
+    r.stuno = "20202030"
+    console.log(r)
+    res.send(JSON.stringify(r))
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
